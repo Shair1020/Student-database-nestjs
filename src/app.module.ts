64f55f82d0fe06614c43b6c7e010393d19b047da
shair1020 <ali99shair@gmail.com>
2024-01-17 12:58:23 +0500

    fix : create crud api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { MONGO_URI } from "./config";
+import { StudentSchema } from "./schema/student.schema";
+import { StudentService } from './student/student.service';
+import { StudentController } from './student/student.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(MONGO_URI), MongooseModule.forFeature([{ name: "Student", schema: StudentSchema }])],
+  controllers: [AppController, StudentController],
+  providers: [AppService, StudentService],
 })
 export class AppModule {}
+
